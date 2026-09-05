dd513ca1e71072d3977936a6f20877738196114f
sunny260420 <sunny260420@gmail.com>
2026-09-05 22:29:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# 0. 【核心新增：解决输入法闪退与剪贴板 ?? 乱码】
+# 1) 创建并显式指定 XDG 运行时目录，给 Fcitx5 搭建好通信总线
+mkdir -p /run/user/0
+export XDG_RUNTIME_DIR=/run/user/0
+chmod 700 /run/user/0
+
+# 2) 强行覆盖全局系统语言环境为中文 UTF-8，彻底打通 GTK3 应用对中文的编解码
+export LANG=zh_CN.UTF-8
+export LC_ALL=zh_CN.UTF-8
 
 # 1. 启动虚拟显示器 (:1) 并给它一点初始化时间
-Xvfb :1 -screen 0 1280x720x24 &
+Xvfb :1 -screen 0 1280x1024x24 &
 sleep 2
 
 # 2. 设置图形环境变量，并在虚拟显示器中拉起 gedit
@@ -20,19 +29,22 @@ openbox-session &
 sleep 1
 # 5. 【强行拉起桌面图标绘制引擎】
 # --desktop 参数会接管壁纸和 root/Desktop 目录下的图标渲染
-pcmanfm --desktop & 
+pcmanfm --desktop & \
+sleep 1
 # 6. 激活剪贴板桥梁
 autocutsel -s CLIPBOARD -fork &
 autocutsel -s PRIMARY -fork &
 # 7. 【核心常驻：在后台默默拉起 fcitx5 输入法进程】
 # -d 表示以守护进程常驻后台，确保它比应用先启动
+
+fcitx5 -d & \
+sleep 1
 terminator &
-fcitx5 -d &
 # 8. 启动默认要弹出的主 GUI 应用（加入关闭 Glycin 沙箱的保底设置）
 
 export GLYCIN_DISABLE_SANDBOX=1
 mousepad &
-firefox &
+# firefox &
 
 sleep 1
 
