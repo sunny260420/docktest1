f8238fc13d5f6bcc5b6fc5f22e1c04754a199025
sunny260420 <sunny260420@gmail.com>
2026-09-05 23:09:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,8 +36,8 @@ sleep 1
 pcmanfm --desktop & \
 sleep 1
 # 6. 激活剪贴板桥梁
-autocutsel -s CLIPBOARD -fork &
-autocutsel -s PRIMARY -fork &
+#autocutsel -s CLIPBOARD -fork &
+#autocutsel -s PRIMARY -fork &
 # 7. 【核心常驻：在后台默默拉起 fcitx5 输入法进程】
 # -d 表示以守护进程常驻后台，确保它比应用先启动
 
