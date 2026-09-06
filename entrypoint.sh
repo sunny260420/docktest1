16ff981972f2a97592d4182bbb7be398652fb501
sunny260420 <sunny260420@gmail.com>
2026-09-06 19:14:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,7 @@ fi
 # 6. 【终极闭环核心：将所有图形和输入法组件整体塞入同一个 dbus 隔离圈】
 # 这会强制让 fcitx5、pcmanfm（负责双击图标）、mousepad 和 Openbox 在启动的第一秒完全共享同一个由 UNIX 套接字生成的、合规的 DBus 会话环境！
 dbus-run-session -- bash -c '
+    export XDG_CONFIG_HOME="/usr/share"
     export XMODIFIERS="@im=fcitx"
     export GTK_IM_MODULE="fcitx"
     export QT_IM_MODULE="fcitx"
@@ -41,7 +42,7 @@ dbus-run-session -- bash -c '
     sleep 1
     pcmanfm --desktop & 
     sleep 1
-    fcitx5 -d & 
+    fcitx5 -d --replace & 
     sleep 1
     xfce4-panel &
     sleep 1
