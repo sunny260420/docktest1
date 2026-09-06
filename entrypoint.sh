64a615af72cd09b56234064d8677a5a0c3cb0b7d
sunny260420 <sunny260420@gmail.com>
2026-09-06 20:17:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,12 +44,12 @@ dbus-run-session -- bash -c '
     sleep 1
     fcitx5 -d --replace & 
     sleep 1
-    xfce4-panel &
+    terminator &
     sleep 1
     export GLYCIN_DISABLE_SANDBOX=1 
     mousepad &
     sleep 1
-    terminator &
+    xfce4-panel &
     sleep 1
     # 保持会话不退出
     wait
