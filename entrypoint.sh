fd29e0557b87b348ac7b1e4a2cb79a6636aeaf37
sunny260420 <sunny260420@gmail.com>
2026-09-06 18:12:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,7 +59,9 @@ sleep 2
 # 7. 启动需密的 VNC 桌面服务端
 x11vnc -forever -shared -display :1 -rfbauth /root/.vnc/passwd -bg && \
 sleep 1
-
+/usr/sbin/sshd && \
+sleep 1
+# AUTO_ACCESS=true PORT=3000 python3 app.py
 # 8. 【最核心的保活】用 exec 让 websockify 成为容器的 1 号主进程 (PID 1)
 # 这样它就会死死钉在前台，绝对不会退出，Railway 的健康检查就能 100% 通过
 exec python3 -m websockify --web /opt/novnc  0.0.0.0:${PORT:-8080} 127.0.0.1:5900
